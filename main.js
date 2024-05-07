9e377806323a8d38c752bf41a6827fc0cc34646e
knightofren2588 <knightofren2488@gmail.com>
2024-05-07 14:51:02 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var kelvin = '293';
+// The above is 293 Kelvin
+var celsius = ((kelvin) - 273);
+// celisus is the Variable and i subtracted Kelvin (273 - 293)
+var fahrenheit = (celsius) * (9/5) + 32;
+// Didnt need to do step 7, 
+// came out to 68 degrees
+console.log(`The Temperature is ${fahrenheit} degrees fahrenheit`);
